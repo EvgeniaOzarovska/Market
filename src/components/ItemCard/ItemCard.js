3adce1e563f49bec709a49252e6788d6444161e3
Evgenia <evgenia.streliaeva@gmail.com>
2022-03-16 11:19:00 +0200

    MRK-1: fixing ItemCard

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -5,7 +5,7 @@ export const ItemCard = props => {
   return (
     <div className={styles.card_block}>
       <h3 className={styles.name}>{props.name}</h3>
-      <img {...props} alt={props.name} src={props.pic} className={styles.card} />
+      <img alt={props.name} src={props.pic} className={styles.card} />
       <p className={styles.price}>{props.price} </p>
       <Button type="button">Buy</Button>
     </div>
